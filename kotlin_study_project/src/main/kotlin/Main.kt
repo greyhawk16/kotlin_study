2e99be705a4f9e3c2f8d7d77e1257b91d0700987
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-07-26 11:14:30 +0900

    코드 변경
    코드 변경

--- kotlin_study_project/src/main/kotlin/Main.kt
+++ kotlin_study_project/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
+
+// https://play.kotlinlang.org/byExample/05_Collections/02_Set
+// 0722: Set 까지 끝냄
+
+
 fun main() {
-    println("Hello World!")
+    val nums = listOf(-3, -2, -1, 0, 1, 2, 3 ,4 ,5)
+    val b = nums.first()
+    println(b)
+    println(nums.last())
 }
